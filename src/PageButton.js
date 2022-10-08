bddf7c47bd478bebc9bf5127c80a69d6b842d19c
Abhishek Sukumar <abhishekskmr44@gmail.com>
2022-10-08 22:05:05 +0530

    pagination

---
+++ src/PageButton.js
@@ -0,0 +1,7 @@
+const PageButton = ({ pg, setPage, isPreviousData }) => {
+    return <button onClick={()=>setPage(pg)} disabled={isPreviousData}>
+        {pg}
+    </button>
+}
+
+export default PageButton
